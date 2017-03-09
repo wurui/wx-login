50a6c86ae0be4fa3adc98f6acbf198d0790daa62
wurui <wurui.wr@gmail.com>
2017-03-09 16:32:15 +0800

    first

--- asset/index.js
+++ asset/index.js
@@ -1,7 +1,27 @@
-define([],function(){
-  return {
-    init:function($mod){
+define([], function () {
+    //https://open.weixin.qq.com/connect/oauth2/authorize?appid=APPID&redirect_uri=REDIRECT_URI&response_type=code&scope=SCOPE&state=STATE#wechat_redirect
 
+    return {
+        init: function ($mod) {
+            var env=$mod.attr('data-env');
+            if(env=='online'){
+                var f=$('form',$mod)[0];
+                if(!f.uid.value){
+                    var appid= f.appid.value;
+                    var state=1;
+                    var scope= f.scope.value||'snsapi_base';
+                    if(scope!='snsapi_userinfo'){
+                        scope='snsapi_base';
+                    }
+                    var redirect_uri=location.protocol+'//'+location.hostname+'/login/oauth_wx?backurl='+encodeURIComponent(location.href)+'&secret='+ f.secret.value+'&appid='+ appid
+
+                    location.href='https://open.weixin.qq.com/connect/oauth2/authorize?appid='+appid+'&redirect_uri='+encodeURIComponent(redirect_uri)+'&response_type=code&scope='+scope+'&state='+state+'#wechat_redirect'
+                }
+            }else{
+                console.log('env',env)
+            }
+
+
+        }
     }
-  }
 })
--- mod.xsl
+++ mod.xsl
@@ -1,13 +1,24 @@
 <xsl:stylesheet version="1.0" xmlns:xsl="http://www.w3.org/1999/XSL/Transform" xmlns:oxm="https://www.openxsl.com">
 
     <xsl:template match="/root" name="wurui.wx-login">
-    <!-- className 'J_OXMod' required  -->
-    <div class="J_OXMod oxmod-wx-login" ox-mod="wx-login">
+        <xsl:param name="appid"/>
+        <xsl:param name="secret"/>
+        <xsl:param name="scope"/>
+        <!-- className 'J_OXMod' required  -->
+        <div class="J_OXMod oxmod-wx-login" ox-mod="wx-login" data-env="{login/env}">
 
-      <h1>
-        This is mod wx-login;
-      </h1>
-      </div>
+
+            <form>
+                <input type="hidden" value="{login/uid}" name="uid"/>
+                <xsl:if test="not(login/uid)">
+                    <p>login...</p>
+                    <input type="hidden" value="{$appid}" name="appid"/>
+                    <input type="hidden" value="{$secret}" name="secret"/>
+                    <input type="hidden" value="{$scope}" name="scope"/>
+                </xsl:if>
+            </form>
+
+        </div>
     </xsl:template>
 
 </xsl:stylesheet>
