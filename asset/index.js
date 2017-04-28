54c3f539b72030fd1c526f348ab318f332b504ba
wurui <wurui.wr@gmail.com>
2017-04-28 19:01:57 +0800

    sss

--- asset/index.js
+++ asset/index.js
@@ -3,6 +3,7 @@ define([], function () {
 
     return {
         init: function ($mod) {
+
             var env=$mod.attr('data-env');
             if(env=='online'){
                 var f=$('form',$mod)[0];
@@ -13,7 +14,7 @@ define([], function () {
                     if(scope!='snsapi_userinfo'){
                         scope='snsapi_base';
                     }
-                    var redirect_uri=location.protocol+'//'+location.hostname+'/login/oauth_wx?backurl='+encodeURIComponent(location.href)+'&secret='+ f.secret.value+'&appid='+ appid
+                    var redirect_uri=location.protocol+'//'+location.hostname+'/export/oauth_wx?backurl='+encodeURIComponent(location.href)+'&secret='+ f.secret.value+'&appid='+ appid
 
                     location.href='https://open.weixin.qq.com/connect/oauth2/authorize?appid='+appid+'&redirect_uri='+encodeURIComponent(redirect_uri)+'&response_type=code&scope='+scope+'&state='+state+'#wechat_redirect'
                 }
